a7fd15e15ac11b68bc363bd2baa487c2d2cc7adc
logesh-newcoder <logeshakn45@gmail.com>
2025-05-23 11:59:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,5 @@ app.post('/api/boys', (req, res) => {
   res.status(201).json({ message: "Product added" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
